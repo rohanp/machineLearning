45d232c9942159638e167094ca06a3b1b2ea8e02
Rohan Pandit <pandit.rohan@gmail.com>
2015-05-20 07:58:49 -0400

    ai for both players

--- tictactoe.py
+++ tictactoe.py
@@ -1,61 +1,60 @@
 __author__ = "Rohan Pandit"
 
 from itertools import product
-import numpy as np
-from random import choice, uniform
+from random import uniform, choice
 import pickle as pkl
 from time import time
 
 def main():
     start = time()
-    #prob = createDict()
-    #pkl.dump(prob, open( 'probability.pkl', 'wb'))
 
-    prob = pkl.load(open('probability.pkl', 'rb'))
+    prob = createDict()
+    oppProb = createDict()
     print("Number of boards: %s"%len(prob.keys()))
 
     print("Training...")
-    for _ in range(10000000):
-        trainingGame(prob)
+    for i in range(1000000):
+        trainingGame(prob, oppProb, i)
 
     print( checkNonzero(prob) )
+    print( checkNonzero(oppProb) )
     pkl.dump(prob, open( 'filled_prob_human_start.pkl', 'wb'))
-
 
     with open('filled_prob_human_start.pkl', 'rb') as f:
         prob = pkl.load(f)
 
     print( "Time elapsed: %s \n"%(time() - start) )
-    playGame(prob)
+    playGame(prob, oppProb)
 
 def createDict():
     prob = {}
-    start = time()
-    entered = 0
 
     for i in product('XO-', repeat=9):
         x = i.count('X')
         o = i.count('O')
-        if x < 5 and o < 5 and abs(x-o) <= 1 and not checkWin(i):
-            prob[''.join(i)] = [9]*9
+        if x < 5 and o < 5 and abs(x-o) <= 1:
+            prob[''.join(i)] = [50]*9
 
     return prob
 
-def trainingGame(prob):
-    board = '-'*9
+def trainingGame(prob, oppProb, gameNum):
+    board = 'O' + '-'*8
     score = [0]*8
-    oppTurn(board, prob, score)  # modifies prob
+    myTurn(board, prob, oppProb, score, gameNum, 1)  # modifies prob
 
-def myTurn(board, prob, score):
-    winner = checkWin(score, board)
-    if winner:
-        return winner
+def myTurn(board, prob, oppProb, score, gameNum, depth):
+    gwinner = checkWin(score, board)
+    if gwinner: return gwinner
 
-    move = choice(getMoves(board))
+    if 100000 < gameNum:
+        move = weighted_choice( getMoves(board), prob[board] )
+    else:
+        move = choice( getMoves(board) )
+
     board = board[:move] + 'X' + board[move + 1:]
     updateScore(score, move, 1)
 
-    winner = oppTurn(board, prob, score)
+    winner = oppTurn(board, prob, oppProb, score, gameNum, depth + 1)
 
     board = board[:move] + '-' + board[move + 1:]
 
@@ -66,22 +65,61 @@ def myTurn(board, prob, score):
     else:
         prob[board][move] += 1
 
+    if prob[board][move] < 0: prob[board][move] = 0
+
     return winner
 
-def oppTurn(board, prob, score):
-    winner = checkWin(score, board)
-    if winner:
-        return winner
+def oppTurn(board, prob, oppProb, score, gameNum, depth):
+    gwinner = checkWin(score, board)
+    if gwinner: return gwinner
 
-    moves = getMoves(board)
-    move = choice(moves)
+    if 100000 < gameNum and 0 < depth:
+        move = weighted_choice( getMoves(board), oppProb[board] )
+    else:
+        move = choice( getMoves(board) )
+
     board = board[:move] + 'O' + board[move + 1:]
     updateScore(score, move, -1)
 
-    return myTurn(board, prob, score)
+    winner = myTurn(board, prob, oppProb, score, gameNum, depth+1)
+
+    board = board[:move] + '-' + board[move + 1:]
+
+    if winner == 'X':
+        oppProb[board][move] -= 1
+    elif winner == 'O':
+        oppProb[board][move] += 3
+    else:
+        oppProb[board][move] += 1
+
+    if oppProb[board][move] < 0: oppProb[board][move] = 0
+
+    return winner
 
 def getMoves(board):
     return [i for i, x in enumerate(board) if x == '-']
+
+
+def weighted_choice_careful(choices, weights):
+    weights = [weights[i] - min(weights) + 1 for i in choices]
+    r = uniform(0, max(weights))
+    upto = 0
+
+    for c, w in zip(choices, weights):
+        if upto + w >= r:
+            return c
+        upto += w
+
+
+def weighted_choice(choices, weights):
+    weights = [weights[i] + 0.4*max(weights) for i in choices]
+    r = uniform(0, max(weights))
+    upto = 0
+
+    for c, w in zip(choices, weights):
+        if upto + w >= r:
+            return c
+        upto += w
 
 def updateScore(score, move, player):
     row = move // 3
@@ -95,7 +133,6 @@ def updateScore(score, move, player):
     if (3 - 1 - col == row):
         score[7] += player
 
-
 def checkNonzero(prob):
     count = 0
     for i in prob.keys():
@@ -106,7 +143,7 @@ def checkNonzero(prob):
 
     return count
 
-def playGame(prob):
+def playGame(prob, oppProb):
     board = '-'*9
     print('Welcome to Tic-Tac Toe! Human Starts')
     print()
@@ -121,7 +158,8 @@ def playGame(prob):
 
         print("Computer turn")
         pprint(board)
-        board = computerTurn(board, prob, score)
+        board = computerTurn(board, prob, oppProb, score)
+        print('opp probs:', oppProb[board])
 
 
     pprint(board)
@@ -129,10 +167,11 @@ def playGame(prob):
     print(checkWin(score, board) + " won the game!")
 
     if input("Would you like to play again? y or n\n") == "y":
-        playGame(prob)
+        playGame(prob, oppProb)
 
-def computerTurn(board, prob, score):
-    move = weighted_choice(getMoves(board), prob[board])
+def computerTurn(board, prob, oppProb, score):
+    move = weighted_choice_careful( getMoves(board), prob[board] )
+
     print('probs: ', prob[board])
     board = board[:move] + 'X' + board[move + 1:]
     print()
@@ -155,16 +194,6 @@ def humanTurn(board, score):
     updateScore(score, move, -1)
     return board
 
-def weighted_choice(choices, weights):
-    weights = [weights[i] + 9 - min(weights) for i in choices]
-    r = uniform(0, max(weights))
-    upto = 0
-
-    for c, w in zip(choices, weights):
-        if upto + w > r:
-            return c
-        upto += w
-
 def checkWin(score, board):
     for i in score:
         if i == 3: return "X"
@@ -173,11 +202,10 @@ def checkWin(score, board):
     if board.count('-') == 0:
         return "NO ONE"
 
-
 def pprint(obj):
-    print( ' '.join([i if i!= '-' else str(j) for i,j in zip(obj[:3], range(3)) ]))
-    print( ' '.join([i if i!= '-' else str(j) for i,j in zip(obj[3:6], range(3,6)) ]))
-    print( ' '.join([i if i!= '-' else str(j) for i,j in zip(obj[6:9], range(6,9)) ]))
+    print( ' '.join([i if i!= '-' else str(j) for i,j in zip(obj[:3], range(3)) ]) )
+    print( ' '.join([i if i!= '-' else str(j) for i,j in zip(obj[3:6], range(3,6)) ]) )
+    print( ' '.join([i if i!= '-' else str(j) for i,j in zip(obj[6:9], range(6,9)) ]) )
 
 if __name__ == "__main__":
    main()
