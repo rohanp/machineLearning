69ceee1756eba78329f4c29f86ac2951f64e552f
Rohan Pandit <pandit.rohan@gmail.com>
2015-05-26 11:16:25 -0400

    5 working

--- ff_neural_network.py
+++ ff_neural_network.py
@@ -5,19 +5,23 @@ from itertools import product
 from random import random, shuffle
 from time import clock
 
-TRIALS = 3000
+TRIALS = 300000
 ALPHA = 0.25 #learning rate
-INPUT = np.array( [ [i, j, k] + [-1] for i, j, k in product( (0,1), repeat=3) ], ndmin=2)
-OUTPUT = { hash( tuple(x) ): x[0] or x[1] for x in INPUT }
+INPUT = np.array( [[0,4], [4,0], [2.1,2.1]] )  #np.array( list( product( (0,1), repeat=2) ) , ndmin=2)
+INPUT = np.c_[INPUT, -1*np.ones( INPUT.shape[0] )].astype(dtype=float) #add bias
+OUTPUT = { hash( tuple(x) ):  4 - x[0] < x[1] for x in INPUT }
+INPUT_TEST =  np.array( [[0,4], [4,0], [2.1, 2.1], [4.1,0], [0,4.1]] )
+INPUT_TEST = np.c_[INPUT_TEST, -1*np.ones( INPUT_TEST.shape[0] )].astype(dtype=float) #add bias
+OUTPUT_TEST = { hash( tuple(x) ):  4 - x[0] < x[1] for x in INPUT_TEST }
 
 def main():
-    weights, epochs = train()
-    verifyNetwork(weights)
-    print("Weights: ", weights)
+    w, epochs = train()
+    print("m = ", -w[0]/w[1], " b = ", w[2]/w[1])
+    verifyNetwork(w)
     print("Epochs: ", epochs)
 
 def train():
-    w = np.random.rand( INPUT.shape[1] )
+    w = 2*np.random.rand( INPUT.shape[1] ) - 1
     epochs = 0
 
     while not trained(w) and epochs < TRIALS:
@@ -26,7 +30,7 @@ def train():
             out = np.dot(x, w) > 0
             target = OUTPUT[ hash( tuple(x) ) ]
 
-            w = w - ALPHA * (out - target) * x
+            w = w - ALPHA * (out - int(target)) * x
             epochs += 1
 
     return w, epochs
@@ -38,9 +42,8 @@ def trained(w):
     return True
 
 def verifyNetwork(w):
-    for x in INPUT:
-        print(x[0], 'or', x[1] , end=" = ")
-        print(np.dot(x, w) > 0)
+    for x in INPUT_TEST:
+        print(x[0], ',', x[1] , " = ", np.dot(x, w) > 0)
     print()
 
 if __name__ == "__main__":
