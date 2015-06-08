19fb0fe17c6620a5a257cd195a27de59bcf2eaef
Rohan Pandit <rohanp@Rohans-MBP.home>
2015-06-08 00:25:14 -0400

    fixed last bug

--- bp_neural_network.py
+++ bp_neural_network.py
@@ -7,34 +7,31 @@ from math import exp
 
 TRIALS = 70000
 ALPHA = 0.25 #learning rate
-INPUT = np.array( list(filter( lambda x: x.count(1) == 1, product( [0,1], repeat=8))) + [-1] )
+INPUT = np.array( list(filter( lambda x: x.count(1) == 1, product( [0,1], repeat=8))) )
+INPUT = np.c_[INPUT, -1*np.ones(INPUT.shape[1])] #add bias
 OUTPUT = { hash( tuple(x) ):  x[:-1] for x in INPUT }
 
 def main():
-    global f
-    f = np.vectorize(f)
     w, v_b, epochs = train()
     verifyNetwork(w, v_b)
     print("\nEpochs: ", epochs)
 
-def train(epochs = 0):
+def train():
+    epochs = 0
     w = 2*np.random.rand( 9,3 ) - 1
     v_b  = 2*np.random.rand( 4, 8 ) - 1
 
     while not trained(w, v_b) and epochs < TRIALS:
         np.random.shuffle(INPUT)
         for x in INPUT:
-            h = f( np.dot(x, w) )
-            h_b = np.r_[h, -1*np.ones(1)] #add bias
-            out = f( np.dot(h_b, v_b) )
-
+            out, h_b = forward(x, w, v_b)
             target = OUTPUT[ hash( tuple(x) ) ]
 
             h_b = h_b[:, np.newaxis] # 4x1
             h, v = h_b[:-1], v_b[:-1,:] # 3x1, 3x8
-            delta = ((out - target) * out * (1 - out))[np.newaxis, :] # 1x8
             x = x[:, np.newaxis] # 9x1
 
+            delta = ((out - target) * out * (1 - out))[np.newaxis, :] # 1x8
             v_b = v_b - ALPHA * np.dot(h_b, delta)
             w = w - ALPHA * np.dot(x, np.dot(delta, v.T) * (h * (1-h)).T)
 
@@ -45,7 +42,7 @@ def train(epochs = 0):
 def forward(x, w, v_b):
     h = f( np.dot(x, w) )
     h_b = np.r_[h, -1*np.ones(1)] #add bias
-    return f( np.dot(h_b, v_b) )
+    return f( np.dot(h_b, v_b) ), h_b
 
 def f(x):
     return 1/(1+exp(-x))
@@ -55,7 +52,7 @@ def fprime(x):
 
 def trained(w, v_b):
     for x in INPUT:
-        out = forward(x, w, v_b)
+        out, h_b = forward(x, w, v_b)
 
         error = sum( 0.5*(x[:-1]-out)**2 )
 
@@ -65,13 +62,15 @@ def trained(w, v_b):
     return True
 
 def verifyNetwork(w, v_b):
-    out = np.array([ forward(x, w, v_b) for x in INPUT ])
+    out = np.array([ forward(x, w, v_b)[0] for x in INPUT ])
     print("Data")
     print("x=\n", INPUT)
     print("out=\n", out)
     print("w=\n", w)
     print("v=\n", v_b)
     print("error=\n", np.add.reduce(0.5*(INPUT[:,:-1]-out)**2,0) )
+
+f = np.vectorize(f)
 
 if __name__ == "__main__":
     start = clock()
