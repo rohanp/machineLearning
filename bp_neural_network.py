5c5eea08c6f9c5ad12d88e64b8caa6fbf12e6e43
Rohan Pandit <rohanp@Rohans-MBP.home>
2015-06-07 23:20:25 -0400

    backpropagation working

--- bp_neural_network.py
+++ bp_neural_network.py
@@ -2,57 +2,76 @@ __author__ = 'Rohan Pandit'
 
 import numpy as np
 from itertools import product
-from random import random, shuffle
 from time import clock
+from math import exp
 
-TRIALS = 50000
+TRIALS = 70000
 ALPHA = 0.25 #learning rate
-INPUT = np.array( filter( lambda x: x.count(1) == 1, list( product( [0,1], repeat=8))) )
-INPUT = np.c_[INPUT, -1*np.ones( INPUT.shape[0] )].astype(dtype=int) #add bias
-OUTPUT = { hash( tuple(x) ):  x[0] ^ x[1] for x in INPUT }
+INPUT = np.array( list(filter( lambda x: x.count(1) == 1, product( [0,1], repeat=8))) + [-1] )
+OUTPUT = { hash( tuple(x) ):  x[:-1] for x in INPUT }
 
 def main():
-    w, v, epochs = train()
-    verifyNetwork(w, v)
+    global f
+    f = np.vectorize(f)
+    w, v_b, epochs = train()
+    verifyNetwork(w, v_b)
     print("\nEpochs: ", epochs)
 
 def train(epochs = 0):
-    w = 2*np.random.rand( 3,2 ) - 1
-    v  = 2*np.random.rand( 3 ) - 1
+    w = 2*np.random.rand( 9,3 ) - 1
+    v_b  = 2*np.random.rand( 4, 8 ) - 1
 
-    while not trained(w, v) and epochs < TRIALS:
+    while not trained(w, v_b) and epochs < TRIALS:
         np.random.shuffle(INPUT)
-        w = np.random.rand( 3,2 ) - 1
-        v  = 2*np.random.rand( 3 ) - 1
-
         for x in INPUT:
-            out = f(x, w, v)
+            h = f( np.dot(x, w) )
+            h_b = np.r_[h, -1*np.ones(1)] #add bias
+            out = f( np.dot(h_b, v_b) )
+
             target = OUTPUT[ hash( tuple(x) ) ]
+
+            h_b = h_b[:, np.newaxis] # 4x1
+            h, v = h_b[:-1], v_b[:-1,:] # 3x1, 3x8
+            delta = ((out - target) * out * (1 - out))[np.newaxis, :] # 1x8
+            x = x[:, np.newaxis] # 9x1
+
+            v_b = v_b - ALPHA * np.dot(h_b, delta)
+            w = w - ALPHA * np.dot(x, np.dot(delta, v.T) * (h * (1-h)).T)
 
             epochs += 1
 
-    return w, v, epochs
+    return w, v_b, epochs
 
-def trained(w, v):
+def forward(x, w, v_b):
+    h = f( np.dot(x, w) )
+    h_b = np.r_[h, -1*np.ones(1)] #add bias
+    return f( np.dot(h_b, v_b) )
+
+def f(x):
+    return 1/(1+exp(-x))
+
+def fprime(x):
+    return f(x)*(1-f(x))
+
+def trained(w, v_b):
     for x in INPUT:
-        if OUTPUT[ hash( tuple(x) ) ] != f(x, w, v):
+        out = forward(x, w, v_b)
+
+        error = sum( 0.5*(x[:-1]-out)**2 )
+
+        if 0.5 < error:
             return False
+
     return True
 
-def verifyNetwork(w, v):
-    for x in INPUT:
-        print(x[0], ',', x[1] , " = ", f(x, w, v))
-    print()
-
-    print("Statistics")
+def verifyNetwork(w, v_b):
+    out = np.array([ forward(x, w, v_b) for x in INPUT ])
+    print("Data")
     print("x=\n", INPUT)
+    print("out=\n", out)
     print("w=\n", w)
-    print("v=\n", v)
-
-def f(x, w, v):
-    h = np.array([ np.dot(x, w[:,0]) > 0, np.dot(x, w[:,1]) > 0, -1])
-    return np.dot(v, h) > 0
-
+    print("v=\n", v_b)
+    print("error=\n", np.add.reduce(0.5*(INPUT[:,:-1]-out)**2,0) )
 
 if __name__ == "__main__":
     start = clock()
